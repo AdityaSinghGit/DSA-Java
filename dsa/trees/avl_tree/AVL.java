8c157ffa07879787b0be59d2bf11655d50cd3342
Aditya Singh <aditya.binary10@gmail.com>
2024-03-26 19:30:39 +0530

    avl tree improved

--- dsa/trees/avl_tree/AVL.java
+++ dsa/trees/avl_tree/AVL.java
@@ -7,47 +7,40 @@ public class AVL {
 
     }
 
-    private class Node {
+    public class Node {
         private int value;
-        private int height;
         private Node left;
         private Node right;
+        private int height;
 
-        Node(int value) {
+        public Node(int value) {
             this.value = value;
         }
 
         public int getValue() {
             return value;
         }
-
     }
 
     public int height() {
-        return getHeight(root);
+        return height(root);
     }
 
-    private int getHeight(Node node) {
+    private int height(Node node) {
         if (node == null) {
             return -1;
         }
         return node.height;
     }
 
-    // Empty check
-    public boolean isEmpty() {
-        return root == null;
-    }
-
-    // Insert
     public void insert(int value) {
         root = insert(value, root);
     }
 
     private Node insert(int value, Node node) {
         if (node == null) {
-            Node newNode = new Node(value);
-            return newNode;
+            node = new Node(value);
+            return node;
         }
 
         if (value < node.value) {
@@ -58,167 +51,114 @@ public class AVL {
             node.right = insert(value, node.right);
         }
 
-        node.height = Math.max(getHeight(node.left), getHeight(node.right)) + 1;
-
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
         return rotate(node);
     }
 
     private Node rotate(Node node) {
-        if ((getHeight(node.left) - getHeight(node.right)) > 1) {
+        if (height(node.left) - height(node.right) > 1) {
             // left heavy
-            if (getHeight(node.left.left) - getHeight(node.left.right) > 0) {
-                // left-left
-                return rotateRight(node);
+            if (height(node.left.left) - height(node.left.right) > 0) {
+                // left left case
+                return rightRotate(node);
             }
-
-            if (getHeight(node.left.left) - getHeight(node.left.right) < 0) {
-                // left-right
-                node.left = rotateLeft(node.left);
-                return rotateRight(node);
+            if (height(node.left.left) - height(node.left.right) < 0) {
+                // left right case
+                node.left = leftRotate(node.left);
+                return rightRotate(node);
             }
-
         }
 
-        if ((getHeight(node.left) - getHeight(node.right)) < -1) {
+        if (height(node.left) - height(node.right) < -1) {
             // right heavy
-            if (getHeight(node.right.left) - getHeight(node.right.right) < 0) {
-                // right-right
-                return rotateLeft(node);
+            if (height(node.right.left) - height(node.right.right) < 0) {
+                // right right case
+                return leftRotate(node);
             }
-
-            if (getHeight(node.right.left) - getHeight(node.right.right) > 0) {
-                // right-left
-                node.right = rotateRight(node.right);
-                return rotateLeft(node);
+            if (height(node.right.left) - height(node.right.right) > 0) {
+                // left right case
+                node.right = rightRotate(node.right);
+                return leftRotate(node);
             }
         }
 
         return node;
     }
 
-    private Node rotateRight(Node p) {
+    public Node rightRotate(Node p) {
         Node c = p.left;
         Node t = c.right;
 
         c.right = p;
         p.left = t;
 
-        p.height = Math.max(getHeight(p.left), getHeight(p.right)) + 1;
-        c.height = Math.max(getHeight(c.left), getHeight(c.right)) + 1;
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
 
         return c;
     }
 
-    private Node rotateLeft(Node c) {
+    public Node leftRotate(Node c) {
         Node p = c.right;
         Node t = p.left;
 
         p.left = c;
         c.right = t;
 
-        p.height = Math.max(getHeight(p.left), getHeight(p.right)) + 1;
-        c.height = Math.max(getHeight(c.left), getHeight(c.right)) + 1;
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
 
         return p;
     }
 
-    // Check if tree is balanced
-    public boolean isBalanced() {
-        return isBalanced(root);
-    }
-
-    private boolean isBalanced(Node node) {
-        if (node == null) {
-            return true;
-        }
-
-        return Math.abs(getHeight(node.left) - getHeight(node.right)) <= 1 && isBalanced(node.left)
-                && isBalanced(node.right);
-    }
-
-    // Add multiple values at once through an array
-    public void populate(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            insert(arr[i]);
+    public void populate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            this.insert(nums[i]);
         }
     }
 
-    // To self balance a sorted array tree
-    public void populateSorted(int[] arr) {
-        populateSorted(arr, 0, arr.length);
+    public void populatedSorted(int[] nums) {
+        populatedSorted(nums, 0, nums.length);
     }
 
-    private void populateSorted(int[] arr, int start, int end) {
+    private void populatedSorted(int[] nums, int start, int end) {
         if (start >= end) {
             return;
         }
 
         int mid = (start + end) / 2;
 
-        insert(arr[mid]);
-        populateSorted(arr, start, mid);
-        populateSorted(arr, mid + 1, end);
+        this.insert(nums[mid]);
+        populatedSorted(nums, start, mid);
+        populatedSorted(nums, mid + 1, end);
     }
 
-    // Display
     public void display() {
-        display(root, "Root node : ");
+        display(this.root, "Root Node: ");
     }
 
     private void display(Node node, String details) {
         if (node == null) {
             return;
         }
-
         System.out.println(details + node.value);
-
-        display(node.left, "Left child of " + node.getValue() + " : ");
-        display(node.right, "Right child of " + node.getValue() + " : ");
+        display(node.left, "Left child of " + node.value + " : ");
+        display(node.right, "Right child of " + node.value + " : ");
     }
 
-    // Pre-Order Traversal
-    public void preOrder() {
-        preOrder(root);
+    public boolean isEmpty() {
+        return root == null;
     }
 
-    private void preOrder(Node node) {
+    public boolean balanced() {
+        return balanced(root);
+    }
+
+    private boolean balanced(Node node) {
         if (node == null) {
-            return;
+            return true;
         }
-
-        System.out.println(node.value + " ");
-        preOrder(node.left);
-        preOrder(node.right);
-    }
-
-    // In-Order Traversal
-    public void inOrder() {
-        inOrder(root);
-    }
-
-    private void inOrder(Node node) {
-        if (node == null) {
-            return;
-        }
-
-        inOrder(node.left);
-        System.out.println(node.value + " ");
-        inOrder(node.right);
-    }
-
-    // Post-Order Traversal
-    public void postOrder() {
-        postOrder(root);
-    }
-
-    private void postOrder(Node node) {
-        if (node == null) {
-            return;
-        }
-
-        postOrder(node.left);
-        postOrder(node.right);
-        System.out.println(node.value + " ");
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
     }
 
     public static void main(String[] args) {
