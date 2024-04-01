cd2baae5b4d980e2ab3a14d6ff96359e2b7fff0a
Aditya Singh <aditya.binary10@gmail.com>
2024-04-01 21:01:10 +0530

    continue dfs questions

--- dsa/trees/questions/DFS.java
+++ dsa/trees/questions/DFS.java
@@ -87,4 +87,60 @@ public class DFS {
         }
     }
     // -----------------X------------X----------X-------------------
+
+    // LeetCode 98
+    // https://leetcode.com/problems/validate-binary-search-tree/description/
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+    }
+
+    private boolean helper(TreeNode node, Integer low, Integer high) {
+        if (node == null) {
+            return true;
+        }
+
+        if (high != null && node.val >= high) {
+            return false;
+        }
+
+        if (low != null && node.val <= low) {
+            return false;
+        }
+
+        boolean left = helper(node.left, low, node.val);
+        boolean right = helper(node.right, node.val, high);
+
+        return left && right;
+    }
+    // -----------------X------------X----------X-------------------
+
+    // LeetCode 230
+    // https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+    int counter = 0;
+
+    public int kthSmallest(TreeNode root, int k) {
+        TreeNode result = helper(root, k);
+        return result.val;
+    }
+
+    private TreeNode helper(TreeNode node, int k) {
+        if (node == null) {
+            return null;
+        }
+
+        TreeNode left = helper(node.left, k);
+
+        if (left != null) {
+            return left;
+        }
+
+        counter++;
+
+        if (counter == k) {
+            return node;
+        }
+
+        return helper(node.right, k);
+    }
+    // -----------------X------------X----------X-------------------
 }
