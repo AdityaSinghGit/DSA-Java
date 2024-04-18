190822fba258321937a2ea5731cfc1f80989b774
Aditya Singh <aditya.binary10@gmail.com>
2024-04-19 00:29:07 +0530

    dialer combination leetcode 17

--- dsa/recursion/strings/combination/LetterCombinationsOfPhoneNumber17.java
+++ dsa/recursion/strings/combination/LetterCombinationsOfPhoneNumber17.java
@@ -1,38 +1,53 @@
 package recursion.strings.combination;
 
+import java.util.*;
+
 public class LetterCombinationsOfPhoneNumber17 {
     public static void main(String[] args) {
-        dialPad("", "12");
+        List<String> ans = letterCombinations("23");
+        System.out.println(ans);
     }
 
-    static void dialPad(String p, String up) {
-        if (up.isEmpty()) {
-            System.out.println(p);
+    public static List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+        if (digits.isEmpty() || digits == null) {
+            return ans;
+        }
+        StringBuilder sb = new StringBuilder();
+        HashMap<Character, String> hm = new HashMap<>();
+
+        hm.put('0', "");
+        hm.put('1', "");
+        hm.put('2', "abc");
+        hm.put('3', "def");
+        hm.put('4', "ghi");
+        hm.put('5', "jkl");
+        hm.put('6', "mno");
+        hm.put('7', "pqrs");
+        hm.put('8', "tuv");
+        hm.put('9', "wxyz");
+
+        helperBacktrack(hm, sb, ans, 0, digits);
+
+        return ans;
+    }
+
+    private static void helperBacktrack(HashMap<Character, String> hm, StringBuilder sb, List<String> ans, int i,
+            String digits) {
+        if (i == digits.length()) {
+            // Once i goes beyond length of digits, store the combination to list.
+            ans.add(sb.toString());
             return;
         }
 
-        int digit = up.charAt(0) - '0'; // This will convert '1' to 1
+        // Store value string based on digit using hashmap
+        String current = hm.get(digits.charAt(i));
+        for (int k = 0; k < current.length(); k++) {
+            sb.append(current.charAt(k)); // Store alphabet based on current
+            helperBacktrack(hm, sb, ans, i + 1, digits);
+            sb.deleteCharAt(sb.length() - 1); // Backtrack
 
-        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
-            char ch = (char) ('a' + i);
-            dialPad(p + ch, up.substring(1));
         }
-
-    }
-
-    static void dialPad(String p, String up) {
-        if (up.isEmpty()) {
-            System.out.println(p);
-            return;
-        }
-
-        int digit = up.charAt(0) - '0'; // This will convert '1' to 1
-
-        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
-            char ch = (char) ('a' + i);
-            dialPad(p + ch, up.substring(1));
-        }
-
     }
 
 }
