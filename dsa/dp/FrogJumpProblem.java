142a42125ed05717c66adb5dd279e0cf54b7725c
Aditya Singh <aditya.binary10@gmail.com>
2024-04-23 18:28:49 +0530

    frog jump

--- dsa/dp/FrogJumpProblem.java
+++ dsa/dp/FrogJumpProblem.java
@@ -5,7 +5,7 @@ public class FrogJumpProblem {
     public static void main(String[] args) {
         int[] arr = { 30, 10, 60, 10, 60, 50 }; // 40
         // int[] arr = { 10, 20, 30, 10 }; //20
-        int ans = frogJump2(2, arr);
+        int ans = frogJump3(2, arr);
         System.out.println(ans);
     }
 
@@ -61,23 +61,21 @@ public class FrogJumpProblem {
 
     // APPROACH 3 - Space Optimization
     public static int frogJump3(int n, int heights[]) {
-        int[] dpArr = new int[heights.length];
+        int prev1 = 0;
+        int prev2 = 0;
 
-        for (int i = 0; i < dpArr.length; i++) {
-            dpArr[i] = -1;
-        }
-
-        dpArr[0] = 0;
-        int fs = Integer.MAX_VALUE;
-        int ss = Integer.MAX_VALUE;
         for (int i = 1; i < heights.length; i++) {
-            fs = dpArr[i - 1] + Math.abs(heights[i] - heights[i - 1]);
+            int fs = prev1 + Math.abs(heights[i] - heights[i - 1]);
+            int ss = Integer.MAX_VALUE;
             if (i > 1)
-                ss = dpArr[i - 2] + Math.abs(heights[i] - heights[i - 2]);
-            dpArr[i] = Math.min(fs, ss);
+                ss = prev2 + Math.abs(heights[i] - heights[i - 2]);
+
+            int curr = Math.min(fs, ss);
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        return dpArr[heights.length - 1];
+        return prev1;
     }
 
 }
