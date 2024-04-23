6a1629d318a3cd0763eac0e0059154dab78e9d76
Aditya Singh <aditya.binary10@gmail.com>
2024-04-23 18:37:51 +0530

    fibo approaches

--- dsa/dp/FiboWithMemoization509.java
+++ dsa/dp/FiboWithMemoization509.java
@@ -1,13 +1,14 @@
 package dp;
 
 public class FiboWithMemoization509 {
-    public static void main(String[] args) {
-        System.out.println((long) fib(40));
-    }
-
     // Leetcode 509
     // https://leetcode.com/problems/fibonacci-number/description/
-    public static int fib(int n) {
+    public static void main(String[] args) {
+        System.out.println((long) fib3(30));
+    }
+
+    // APPROACH 1 - Recursion & Memoization
+    public static int fib1(int n) {
         int[] dpArr = new int[n + 1];
 
         for (int i = 0; i < dpArr.length; i++) {
@@ -29,5 +30,37 @@ public class FiboWithMemoization509 {
         dpArr[n] = findFibo(n - 1, dpArr) + findFibo(n - 2, dpArr);
 
         return dpArr[n];
+    }
+
+    // APPROACH 2 - Tabulation
+    public static int fib2(int n) {
+        int[] dpArr = new int[n + 1];
+
+        for (int i = 0; i < dpArr.length; i++) {
+            dpArr[i] = -1;
+        }
+
+        dpArr[0] = 0;
+        dpArr[1] = 1;
+        for (int i = 2; i < dpArr.length; i++) {
+            dpArr[i] = dpArr[i - 1] + dpArr[i - 2];
+        }
+
+        return dpArr[n];
+    }
+
+    // APPROACH 3 - Space Optimization
+    public static int fib3(int n) {
+
+        int prev1 = 1;
+        int prev2 = 0;
+
+        for (int i = 2; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
     }
 }
