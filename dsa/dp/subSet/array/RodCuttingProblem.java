c20ff77cc6da760468a8b8456e35fb9b6ff378e8
Aditya Singh <aditya.binary10@gmail.com>
2024-07-26 13:44:14 +0530

    dp practice

--- dsa/dp/subSet/array/RodCuttingProblem.java
+++ dsa/dp/subSet/array/RodCuttingProblem.java
@@ -12,60 +12,90 @@ public class RodCuttingProblem {
         System.out.println(ans);
     }
 
-    // APPROACH 1 : Recursion and Modulation
-    public static int cutRod(int price[], int n) {
-        int[][] dpArr = new int[price.length][n + 1];
+    // TC: O(exponential)
+    // SC: O(Target)
 
-        for (int[] rows : dpArr) {
+    // APPROACH 1 : Recursion and Modulation
+    // TC: O(N x M)
+    // SC: O(N X M) + O(Target)
+    public static int cutRod(int price[], int n) {
+        int[][] dp = new int[price.length][n + 1];
+
+        for (int[] rows : dp) {
             Arrays.fill(rows, -1);
         }
 
-        return helper(price.length - 1, n, price, dpArr);
+        return helper(price.length - 1, n, price, dp);
     }
 
-    private static int helper(int index, int target, int[] price, int[][] dpArr) {
-        if (index == 0) {
+    private static int helper(int ind, int target, int[] price, int[][] dp) {
+        if (ind == 0) {
             return target * price[0];
         }
 
-        if (dpArr[index][target] != -1) {
-            return dpArr[index][target];
+        if (dp[ind][target] != -1) {
+            return dp[ind][target];
         }
 
-        int notTake = 0 + helper(index - 1, target, price, dpArr);
+        int notTake = helper(ind - 1, target, price, dp);
         int take = Integer.MIN_VALUE;
-        if (target >= (index + 1)) {
-            take = price[index] + helper(index, target - (index + 1), price, dpArr);
+        if (target >= ind + 1) {
+            take = helper(ind, target - (ind + 1), price, dp) + price[ind];
         }
 
-        dpArr[index][target] = Math.max(take, notTake);
-        return dpArr[index][target];
+        return dp[ind][target] = Math.max(take, notTake);
     }
 
     // APPROACH 2 : Tabulation
-    public static int cutRod2(int price[], int n) {
-        int[][] dpArr = new int[price.length][n + 1];
+    // TC: O(N x M)
+    // SC: O(N X M)
+    public static int cutRod1(int price[], int n) {
+        int[][] dp = new int[price.length][n + 1];
 
-        for (int[] rows : dpArr) {
-            Arrays.fill(rows, 0);
+        for (int target = 1; target <= n; target++) {
+            dp[0][target] = target * price[0];
         }
 
-        for (int target = 0; target <= n; target++) {
-            dpArr[0][target] = target * price[0];
-        }
-
-        for (int index = 1; index < n; index++) {
-            for (int target = 0; target <= n; target++) {
-                int notTake = dpArr[index - 1][target];
+        for (int ind = 1; ind < price.length; ind++) {
+            for (int target = 1; target <= n; target++) {
+                int notTake = dp[ind - 1][target];
                 int take = Integer.MIN_VALUE;
-                if (target >= (index + 1)) {
-                    take = price[index] + dpArr[index][target - (index + 1)];
+                if (target >= ind + 1) {
+                    take = dp[ind][target - (ind + 1)] + price[ind];
                 }
-                dpArr[index][target] = Math.max(notTake, take);
+
+                dp[ind][target] = Math.max(take, notTake);
             }
         }
 
-        return dpArr[price.length - 1][n];
+        return dp[price.length - 1][n];
+    }
+
+    // APPROACH 3 : Space opt
+    // TC: O(N x M)
+    // SC: O(N)
+    public static int cutRod2(int price[], int n) {
+        int[] prev = new int[n + 1];
+
+        for (int target = 1; target <= n; target++) {
+            prev[target] = target * price[0];
+        }
+
+        for (int ind = 1; ind < price.length; ind++) {
+            int[] curr = new int[n + 1];
+            for (int target = 1; target <= n; target++) {
+                int notTake = prev[target];
+                int take = Integer.MIN_VALUE;
+                if (target >= ind + 1) {
+                    take = curr[target - (ind + 1)] + price[ind];
+                }
+
+                curr[target] = Math.max(take, notTake);
+            }
+            prev = curr;
+        }
+
+        return prev[n];
     }
 
 }
