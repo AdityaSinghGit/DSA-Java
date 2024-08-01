c7027e4e06d2c1df9ce815db78d9a2ba118a4528
Aditya Singh <aditya.binary10@gmail.com>
2024-08-01 23:57:31 +0530

    lld improv

---
+++ systemDesign/LLD/Designs/O4_ParkingLot/Parking.java
@@ -0,0 +1,42 @@
+// package systemDesign.LLD.Designs.O4_ParkingLot;
+
+// import java.util.List;
+
+// public class Parking {
+// private List<Integer> id;
+// private VehicleType type;
+// private int distance;
+// Entry entry;
+
+// public Parking(List<Integer> id, VehicleType type, int distance) {
+// this.id = id;
+// this.type = type;
+// this.distance = Math.abs(entry.getCoordinates().get(0) - id.get(0))
+// + Math.abs(entry.getCoordinates().get(1) - id.get(1));
+// }
+
+// public List<Integer> getId() {
+// return id;
+// }
+
+// public VehicleType getType() {
+// return type;
+// }
+
+// public void setType(VehicleType type) {
+// this.type = type;
+// }
+
+// public int getDistance() {
+// return distance;
+// }
+
+// public Entry getEntry() {
+// return entry;
+// }
+
+// public void setEntry(Entry entry) {
+// this.entry = entry;
+// }
+
+// }
---
+++ systemDesign/LLD/Designs/O4_ParkingLot/ParkingManager.java
@@ -0,0 +1,20 @@
+// package systemDesign.LLD.Designs.O4_ParkingLot;
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+// public class ParkingManager {
+// private List<Parking> parkingLot;
+
+// public ParkingManager() {
+// parkingLot = new ArrayList<>();
+// for(int i = 0; i < 10; i++){
+// for(int j = 0; j < 10; j++){
+// Parking parkingSpot = new Parking();
+// }
+// }
+// }
+
+// // public void add
+
+// }
