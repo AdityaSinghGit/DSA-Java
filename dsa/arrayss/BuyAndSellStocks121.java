7b85ad935528d0912cb6a50a7e526bc74357cdd9
Aditya Singh <aditya.binary10@gmail.com>
2024-08-26 18:34:17 +0530

    leetcode practice

--- dsa/arrayss/BuyAndSellStocks121.java
+++ dsa/arrayss/BuyAndSellStocks121.java
@@ -2,50 +2,35 @@ package arrayss;
 
 public class BuyAndSellStocks121 {
     public static void main(String[] args) {
-        int[] arr = { 3, 2, 6, 5, 0, 3 };
-        System.out.println(maxProfit(arr));
-        System.out.println(maxProfitSlidingWindow(arr));
-    }
-
-    public static int maxProfit(int[] prices) {
-        int buy = prices[0];
-        int sell = prices[0];
-        int profit = 0;
-
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < buy) {
-                buy = prices[i];
-                sell = prices[i];
-            }
-
-            if (prices[i] > sell) {
-                sell = prices[i];
-                int current = sell - buy;
-                if (profit < current) {
-                    profit = current;
-                }
-            }
-        }
-
-        return profit;
+        int[] arr = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(maxProfitModifiedResult(arr));
     }
 
     public static int maxProfitSlidingWindow(int[] prices) {
-        int buy = 0;
-        int sell = 1;
-        int maxProfit = 0;
-
-        while (sell < prices.length) {
-            int currentProfit = 0;
-            if (prices[buy] < prices[sell]) {
-                currentProfit = prices[sell] - prices[buy];
-                maxProfit = Math.max(maxProfit, currentProfit);
-            } else {
-                buy = sell;
-            }
-            sell++;
+        int buy = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int cost = prices[i] - buy;
+            profit = Math.max(profit, cost);
+            buy = Math.min(buy, prices[i]);
         }
+        return profit;
+    }
 
-        return maxProfit;
+    public static String maxProfitModifiedResult(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+        int buyDay = 0;
+        int sellDay = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int cost = prices[i] - buy;
+            if (cost > profit)
+                sellDay = i;
+            profit = Math.max(profit, cost);
+            if (prices[i] < buy)
+                buyDay = i;
+            buy = Math.min(buy, prices[i]);
+        }
+        return "Bought at day " + buyDay + " and sold at day " + sellDay;
     }
 }
